9995218834ad0584ceda09e7f0c1f9241a503293
SLcode777 <paperstreet.notion@gmail.com>
2025-02-09 15:22:27 +0100

    exercise 1 completed

--- pages/exercices/1.tsx
+++ pages/exercices/1.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
+import { z } from 'zod';
 import { Loader } from '~/components/Loader';
 import type { TlTweets } from '~/lib/scheme/tweets';
 import { AddTweetForm } from '../../src/components/tweets/AddTweetForm';
@@ -13,7 +14,27 @@ const notifyFailed = () => toast.error("Couldn't fetch tweet...");
 // 🦁 Créer un schéma zod appelé TweetsScheme qui correspond à la réponse de l'API
 // Tu peux `console.log` la réponse de l'API pour voir la structure attendue
 // Tu pourrais utiliser zod transform pour modifier directement dans le schéma la date
-// 💡 const TweetsScheme = z.object({...
+const TweetsScheme = z.object({
+  tweets: z.array(
+    z.object({
+      id: z.string(),
+      content: z.string(),
+      createdAt: z.string().datetime(),
+      user: z.object({
+        id: z.string(),
+        displayName: z.string().nullable().default(null),
+        username: z.string(),
+        avatarUrl: z.string().url().nullable().default(null),
+      }),
+      likes: z.array(z.string()),
+      _count: z.object({
+        likes: z.number(),
+        replies: z.number(),
+      }),
+      liked: z.boolean(),
+    })
+  ),
+});
 
 export default function FetchAllTweets() {
   const [tweets, setTweets] = useState<TlTweets | null>(null);
@@ -21,17 +42,33 @@ export default function FetchAllTweets() {
   useEffect(() => {
     // 🦁 Créer un abort controller pour annuler la requête si l'utilisateur quitte la page
 
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     // 🦁 Passer le signal à la requête fetch
-    fetch('/api/tweets') // ℹ️ tu peux remplacer l'url par `/api/tweets?error=erreur` pour voir le problème
+    fetch('/api/tweets', { signal }) // ℹ️ tu peux remplacer l'url par `/api/tweets?error=erreur` pour voir le problème
       .then((res) => res.json())
       .then((data) => {
         // 🦁 Utiliser le schéma TweetsScheme pour valider la réponse de l'API
+        const parsed = TweetsScheme.safeParse(data);
 
-        setTweets(data.tweets);
+        if (!parsed.success) {
+          console.error('Erreur de validation Zod:', parsed.error);
+          notifyFailed();
+          return;
+        }
+
+        setTweets(parsed.data.tweets);
       })
-      .catch(() => void 0); // 🦁 Ajouter un catch pour gérer les erreurs
+      .catch((error) => {
+        if (error.name !== 'AbortError') {
+          console.error('Erreur Fetch', error);
+          notifyFailed(); // 🦁 Ajouter un catch pour gérer les erreurs
+        }
+      });
 
     // 🦁 Créer la cleanup fonction qui annule la requête
+    return () => controller.abort();
   }, []);
 
   if (!tweets) return <Loader />;
